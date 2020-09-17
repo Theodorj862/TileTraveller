ad2d5d99988436bb59c3a2467836aa63b22958c9
Theodorj862 <theodor19@ru.is>
2020-09-17 14:15:02 +0000

    first commit

---
+++ Assignment_python.py
@@ -0,0 +1,14 @@
+# Erum með 3x3 ferning þar sem markmiðið er að byrja á einum enda og enda á hinum og leiðbeina notandanum í gegnum brautina að endastöð
+# Notum eina stóra while lykkju sem runnar á meðan notandi er ekki á endastöð. Eftir hverja færslu fær notandi möguleika um leiðir sem hægt er að fara þaðan
+# og á næstu stöð. Ef notandi slær inn invalid færslu kemur upp invalid í runninu og notandi látinn reyna aftur frá þar sem hann failaði.
+# Forritið mun búa til tvö int gildi og þau breytast eftir valmöguleikum og gefa þannig forritara nákvæma staðsetningu notanda.
+# Hver reitur hefur upplýsingar um staðsetningu og hvert er hægt að fara af honum og if- og elif setningar sem marka hvert má og hvert notandi fer
+# Ein stór while lykkja sem keyrist ef notandi er ekki á lokareitnum og inni í henni verða 8 while lykkjur fyrir hvern reit og ef notandi lendir a stóra reitnum
+# Breakar while lykkjan og leikurinn klárast
+while a != 3 and b != 1
+
+
+
+
+if a = 3 and b = 1
+    print('game over')
